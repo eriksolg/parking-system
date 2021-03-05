7065171e79c323d2fb38ca4c0fef3bde0553586e
eriksol <erik.solg@tallink.ee>
2021-03-05 18:22:32 +0200

    Database initialization. Repositories.

--- parking/src/main/java/com/veebzone/parking/controller/ParkingLotController.java
+++ parking/src/main/java/com/veebzone/parking/controller/ParkingLotController.java
@@ -1,8 +1,33 @@
 package com.veebzone.parking.controller;
 
+import com.veebzone.parking.model.Floor;
+import com.veebzone.parking.model.Registration;
+import com.veebzone.parking.model.Slot;
+import com.veebzone.parking.repository.FloorRepository;
+import com.veebzone.parking.repository.RegistrationRepository;
+import com.veebzone.parking.repository.SlotRepository;
+import com.veebzone.parking.service.ParkingLotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ParkingLotController {
+
+    @Autowired
+    ParkingLotService parkingLotService;
+
+    @GetMapping("/floors")
+    public List<Floor> getAllFloors() {
+        return parkingLotService.getAllFloors();
+    }
+
+    @GetMapping("/floors/{id}/slots")
+    public List<Slot> getSlotsOnFloor(@PathVariable Long id) {
+        return parkingLotService.getSlotsOnFloor(id);
+    }
 
 }
--- parking/src/main/java/com/veebzone/parking/service/ParkingLotService.java
+++ parking/src/main/java/com/veebzone/parking/service/ParkingLotService.java
@@ -1,7 +1,28 @@
 package com.veebzone.parking.service;
 
+import com.veebzone.parking.model.Floor;
+import com.veebzone.parking.model.Slot;
+import com.veebzone.parking.repository.FloorRepository;
+import com.veebzone.parking.repository.SlotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ParkingLotService {
+
+    @Autowired
+    FloorRepository floorRepository;
+
+    @Autowired
+    SlotRepository slotRepository;
+
+    public List<Floor> getAllFloors() {
+        return floorRepository.findAll();
+    }
+
+    public List<Slot> getSlotsOnFloor(Long id) {
+        return slotRepository.findAll();
+    }
 }
