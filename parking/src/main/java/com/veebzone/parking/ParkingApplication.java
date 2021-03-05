0517bae7356d0472c70435b618f53e55d387fc9f
eriksol <erik.solg@tallink.ee>
2021-03-05 22:58:03 +0200

    Added frontend

--- parking/src/main/java/com/veebzone/parking/ParkingApplication.java
+++ parking/src/main/java/com/veebzone/parking/ParkingApplication.java
@@ -2,13 +2,19 @@ package com.veebzone.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ParkingApplication {
+public class ParkingApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ParkingApplication.class);
 	}
 
 }
